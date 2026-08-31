eae79ddcbb1630a493130e7b90097773527feb80
Tom Cloyd <tc@tomcloyd.com>
2026-08-31 10:21:32 -0700

    test

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION = "2026-08-31:1015"
+VERSION = "2026-08-31:1020"
 
 # A short, plain-language note shown next to VERSION on the home page and
 # used to describe this build in the update-available banner (see
@@ -9,4 +9,4 @@ VERSION = "2026-08-31:1015"
 #   "minor problem fixes"
 #   "minor function added or improved"
 #   "significant improvements implemented — see top of User Manual for details"
-VERSION_NOTE = "moved the version-note box up top, below the update banner"
+VERSION_NOTE = "minor problem fixes: test of new update msg"
