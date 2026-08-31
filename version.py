b71b644c3d1730f45c9cecdb77e3484bf59ebaf5
Tom Cloyd <tc@tomcloyd.com>
2026-08-31 10:06:56 -0700

    test

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-VERSION = "2026-08-31:0946"
+VERSION = "2026-08-31:1006"
 
 # A short, plain-language note shown next to VERSION on the home page and
 # used to describe this build in the update-available banner (see
